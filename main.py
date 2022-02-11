b8b7e6119931edd3e7e0047bcbf052954b5300d0
BuildTools <unconfigured@null.spigotmc.org>
2022-02-11 20:34:18 +0100

    Added video downloads (now works), fixed threading and created new functions

--- main.py
+++ main.py
@@ -1,62 +1,123 @@
-#!/usr/binenv python3
+from ast import arg
 from pytube import YouTube
 import os
 import datetime
 import requests
 import time
 import json
+from threading import Thread
+import concurrent.futures
 
-def create_folder(YT_TITLE, YT_TH_URL):
-    P_DIR = '/Users/johannesfalnes/PycharmProjects/YT-Archiver/Downloads'
-    STR_TITLE = YT_TITLE
-    PATH = os.path.join(P_DIR, STR_TITLE)
-    os.mkdir(PATH)
-    print(PATH)
-    create_text(YT_TITLE=YT_TITLE, PATH=PATH)
-    time.sleep(0.5)
-    download_thumb(YT_TH_URL=YT_TH_URL, YT_TITLE=YT_TITLE, PATH=PATH)
-    download_video(YT_TITLE, PATH)
+def start():
+
+    global yt
+
+    YT_LINK = input('Link to video: ')
+    P_DIR = input('Full path to directory to save to: ')
+    yt = YouTube(YT_LINK)
+
+    YT_TITLE = yt.title
+
+    # Get the YouTube Thumbnail URL
+    YT_TH_URL = yt.thumbnail_url
+    PATH = os.path.join(P_DIR, YT_TITLE)
+
+    # Going to make a solution for 'illegal' characters here
+    ILLEGAL_CHAR = ['<', '>', ':', '"', '/', '\\', '|', '?', '*']
+    for x in ILLEGAL_CHAR:
+        if x in YT_TITLE:
+            YT_TITLE = YT_TITLE.replace(x, '')
+
+    pairs = [
+        (create_folder, (YT_TITLE, PATH,)),
+        (create_text, (YT_TITLE, PATH,)),
+        (download_thumb, (YT_TH_URL, YT_TITLE, PATH,)),
+        (download_video, (YT_LINK, PATH, YT_TITLE,))
+    ]
+
+    threads = [Thread(target=func, args=args) for func, args in pairs]
+
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    N_VIDEO = input('New video? y/n')
+    while N_VIDEO != 'n':
+        start()
+
+def create_folder(YT_TITLE, PATH):
+    # Needs to include full path to directory for now
+
+    #P_DIR = '/Users/johannesfalnes/PycharmProjects/YT-Archiver/Downloads'
+
+    if os.path.isdir(PATH):
+        print(f'Folder already exists in path {PATH}. Bypassing.')
+    else:
+        print(f'Creating folder in {PATH}')
+        os.mkdir(PATH)
 
 
 
 def create_text(YT_TITLE, PATH):
-    time_downloaded = datetime.datetime.now()
-    file_entries = [YT_TITLE, time_downloaded]
     N_TITLE = YT_TITLE + '.txt'
+
     my_file = (PATH + '/' + N_TITLE)
-    write_to_file = {'Title': YT_TITLE,
-                     'Channel URL': yt.channel_url,
-                     'Video Views': yt.views,
-                     'Video Desc': yt.description,
-                     'Length': yt.length,
-                     'Publication Date': yt.publish_date,
-                     'Rating': yt.rating,
-                     'Time Downloaded': str(time_downloaded)
-                     }
-    with open(os.path.join(PATH, my_file), 'w') as f:
-        f.write(json.dumps(write_to_file, indent=4, sort_keys=False, default=str))
+    FILE_PATH = os.path.join(PATH, my_file)
+
+    if os.path.isfile(FILE_PATH):
+        print(f'File already exists in path {FILE_PATH}, bypassing...')
+    else:
+        print(f'Creating stats file in {FILE_PATH}')
+        time_downloaded = datetime.datetime.now()
+        write_to_file = {'Title': YT_TITLE,
+                        'Channel URL': yt.channel_url,
+                        'Video Views': yt.views,
+                        'Video Desc': yt.description,
+                        'Video ID': yt.video_id,
+                        'Length': yt.length,
+                        'Publication Date': yt.publish_date,
+                        'Rating': yt.rating,
+                        'Time Downloaded': time_downloaded,
+                        'Age restricted': yt.age_restricted
+                        }
+
+        with open(FILE_PATH, 'w') as f:
+            f.write(json.dumps(write_to_file, indent=4, sort_keys=False, default=str))
+
 
 
 def download_thumb(YT_TH_URL, YT_TITLE, PATH):
     response = requests.get(YT_TH_URL)
-    print(PATH)
-    with open(os.path.join(PATH, (YT_TITLE + '.jpg')), 'wb') as f:
-        f.write(response.content)
-        f.close()
+
+    THUMB_PATH = os.path.join(PATH, (YT_TITLE + '.jpg'))
+    if os.path.isfile(THUMB_PATH):
+        print(f'Thumbnail already exists in path: {THUMB_PATH}. Bypassing')
+    else:
+        print(f'Downloading thumbnail to {THUMB_PATH}')
+        with open(THUMB_PATH, 'wb') as f:
+            f.write(response.content)
+            f.close()
 
 
-def download_video(YT_LINK, PATH):
-    yt = YouTube(YT_LINK)
-    yt = yt.get('mp4', '720p')
-    yt.download(PATH)
-    
 
-dl_path = 'Downloads'
-YT_LINK = 'https://www.youtube.com/watch?v=OvKNTaySrsE/'
-yt = YouTube(YT_LINK)
+def download_video(YT_LINK, PATH, YT_TITLE):
 
-YT_TITLE = yt.title
+    try:
+        VIDEO_PATH = os.path.join(PATH, (YT_TITLE + '.mp4'))
+        print(VIDEO_PATH)
+        if os.path.isfile(VIDEO_PATH):
+            print(f'Video already exists in path: {VIDEO_PATH}. Bypassing')
+        else:
+            print('Downloading video ', YT_LINK)
 
-# Get the YouTube Thumbnail URL
-YT_TH_URL = yt.thumbnail_url
-create_folder(YT_TITLE=YT_TITLE, YT_TH_URL=YT_TH_URL)
+            yt = YouTube(YT_LINK)
+            video = yt.streams.get_highest_resolution()
+            video.download(PATH)
+    except:
+        print(f'Something went wrong while downloading')
+
+
+if __name__ == '__main__':
+    start()
